8c45de32fb648423ab978c54eb3747d89fa992ff
lq186 <liuqing4public@sina.com>
2018-09-09 10:20:02 +0800

    add token to header

--- lq186.com/apiserver/filter/token_filter.go
+++ lq186.com/apiserver/filter/token_filter.go
@@ -1,28 +1,15 @@
 package filter
 
 import (
-	"github.com/lq186/golang/lq186.com/apiserver/log"
-	"github.com/lq186/golang/lq186.com/apiserver/response"
-	"net/http"
-	"github.com/lq186/golang/lq186.com/apiserver/user"
 	"github.com/lq186/golang/lq186.com/apiserver/common"
+	"github.com/lq186/golang/lq186.com/apiserver/response"
+	"github.com/lq186/golang/lq186.com/apiserver/user"
+	"net/http"
 )
 
 func TokenHandle(w http.ResponseWriter, r *http.Request, data map[string]interface{}) bool {
 
-	err := r.ParseForm()
-	if err != nil {
-		log.Log.Errorf("Can not parse request form, More info: %v", err)
-		response.WriteJsonData(w, response.Data{Code:response.SystemError, Message:err.Error()})
-		return false
-	}
-
-	if len(r.Form["token"]) == 0 {
-		response.WriteJsonData(w, response.Data{Code:response.TokenError, Message: "No token found."})
-		return false
-	}
-
-	token := r.Form["token"][0]
+	token := r.Header.Get("Token")
 	if "" == token {
 		response.WriteJsonData(w, response.Data{Code:response.TokenError, Message: "No token found."})
 		return false
